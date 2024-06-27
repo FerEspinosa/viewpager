4f43d63f116592355df8e05082f5542bdba7decf
Fernando Espinosa <elmundodefer@gmail.com>
2024-06-27 14:43:27 -0300

    added TabLayout for interacting with viewPager and its fragments

--- app/src/main/java/com/curso/android/app/practica/viewpagerapp/MainActivity.java
+++ app/src/main/java/com/curso/android/app/practica/viewpagerapp/MainActivity.java
@@ -3,16 +3,22 @@ package com.curso.android.app.practica.viewpagerapp;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 viewpager;
     MyViewPagerAdapter myAdapter;
+
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tabLayout = findViewById(R.id.tabLayout);
 
         myAdapter = new MyViewPagerAdapter(
                 getSupportFragmentManager(),
@@ -39,5 +47,18 @@ public class MainActivity extends AppCompatActivity {
         viewpager = findViewById(R.id.viewPager2);
         viewpager.setOrientation(viewpager.ORIENTATION_HORIZONTAL);
         viewpager.setAdapter(myAdapter);
+
+        // connect tabLayout with viewPager
+        new TabLayoutMediator(
+                tabLayout,
+                viewpager,
+                new TabLayoutMediator.TabConfigurationStrategy(){
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Fragment "+(position+1));
+                    }
+                }
+        ).attach();
+
     }
 }
